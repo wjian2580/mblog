f198d170ca38f480bc3fc67eb0113fa7259445b8
unknown <>
2017-08-24 23:51:18 +0800

    hehe

--- app/views.py
+++ app/views.py
@@ -30,7 +30,7 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
         return redirect('/index')
-    else:  
-        return render_template('login.html',
-            title = 'Sign In',
-            form = form)
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
