1cf44d5173df7f52278baa83c627c9783684b35f
unknown <>
2017-08-27 23:50:14 +0800

    db

---
+++ app/__pycache__/test.py
@@ -0,0 +1,8 @@
+import sys
+print(sys.path)
+class Mytest(object):
+	def add(a,b):
+		return a+b
+	def sub(a,b):
+		return a-b
+		
--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import Form
 from wtforms import StringField,BooleanField
 from wtforms.validators import DataRequired
 
-#创建登陆表单（通过openid校验）
+# 创建登陆表单（通过openid校验）
 class LoginForm(Form):
 	openid = StringField('openid',validators=[DataRequired()])
 	remember_me = BooleanField('remember_me',default=False)
