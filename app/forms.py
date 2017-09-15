6ef3854780ae0aa24d73e35143cf6c4acc0f5e49
Jian.Wang <wjian2580@163.com>
2017-09-15 08:36:28 +0800

    'utf-8'

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
+# -*- encoding: utf-8 -*-
 from flask_wtf import Form
 from wtforms import StringField,BooleanField
 from wtforms.validators import DataRequired
 
-# 创建登陆表单（通过openid校验）
+# åå»ºç»éè¡¨åï¼éè¿openidæ ¡éªï¼
 class LoginForm(Form):
 	openid = StringField('openid',validators=[DataRequired()])
 	remember_me = BooleanField('remember_me',default=False)
